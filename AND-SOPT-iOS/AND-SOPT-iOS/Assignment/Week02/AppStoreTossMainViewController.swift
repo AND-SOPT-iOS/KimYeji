81f7880d0dff30626dafb8bf3b89a876104a7189
Yeji <mnbvcxzyj@gmail.com>
2024-10-21 21:03:50 +0900

    feat: 탭하여 평가, 사용자 리뷰 UI 구현

--- AND-SOPT-iOS/AND-SOPT-iOS/Assignment/Week02/AppStoreTossMainViewController.swift
+++ AND-SOPT-iOS/AND-SOPT-iOS/Assignment/Week02/AppStoreTossMainViewController.swift
@@ -299,7 +299,7 @@ class AppStoreTossMainViewController: UIViewController {
         
         return label
     }()
-
+    
     
     private let appInfoLabel: UILabel = {
         let label = UILabel()
@@ -352,7 +352,165 @@ class AppStoreTossMainViewController: UIViewController {
         
         return view
     }()
-
+    
+    // MARK: 여섯번째 섹션 (평가 및 리뷰, 리뷰 내용) - 마지막
+    private let ratingAndReviewLabel: UILabel = {
+        let label = UILabel()
+        label.text = "평가 및 리뷰"
+        label.font = .systemFont(ofSize: 20, weight: .bold)
+        label.textColor = .white
+        
+        return label
+    }()
+    
+    private let seeAllReviewsButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("모두 보기", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 15, weight: .semibold)
+        
+        return button
+    }()
+    
+    private let ratingScoreLabel: UILabel = {
+        let label = UILabel()
+        label.text = "4.4"
+        label.font = .systemFont(ofSize: 55, weight: .black)
+        label.textColor = .white
+        
+        return label
+    }()
+    
+    private let maxRatingLabel: UILabel = {
+        let label = UILabel()
+        label.text = "5점 만점"
+        label.font = .systemFont(ofSize: 14, weight: .medium)
+        label.textColor = .lightGray
+        
+        return label
+    }()
+    
+    private let reviewCountsLabel: UILabel = {
+        let label = UILabel()
+        label.text = "8.4만개의 평가"
+        label.font = .systemFont(ofSize: 14, weight: .medium)
+        label.textColor = .lightGray
+        
+        return label
+    }()
+    
+    private let separateLine6: UIView = {
+        let view = UIView()
+        view.backgroundColor = .lightGray
+        
+        return view
+    }()
+    
+    // 리뷰
+    private let tapToRateLabel: UILabel = {
+        let label = UILabel()
+        label.text = "탭하여 평가하기 : "
+        label.font = .systemFont(ofSize: 18, weight: .semibold)
+        label.textColor = .white
+        
+        return label
+    }()
+    
+    private let rateStarStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 10
+        stackView.alignment = .center
+        stackView.distribution = .fillEqually
+        return stackView
+    }();
+    
+    // 리뷰 내용
+    private let reviewBackgroundGrayView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .darkGray
+        view.layer.cornerRadius = 10
+        view.layer.masksToBounds = true
+        return view
+    }()
+    
+    private let reviewTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "김예지(리뷰제목)"
+        label.font = .systemFont(ofSize: 16, weight: .bold)
+        label.textColor = .white
+        return label
+    }()
+    
+    private let reviewDateLabel: UILabel = {
+        let label = UILabel()
+        label.text = "10월 21일"
+        label.font = .systemFont(ofSize: 14, weight: .semibold)
+        label.textColor = .lightGray
+        return label
+    }()
+    
+    private let userNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "ENFJ"
+        label.font = .systemFont(ofSize: 14, weight: .semibold)
+        label.textColor = .lightGray
+        return label
+    }()
+    
+    private let reviewStarScoreLabel: UILabel = {
+        let label = UILabel()
+        label.text = "★★★★★"
+        label.font = .systemFont(ofSize: 14, weight: .semibold)
+        label.textColor = .orange
+        return label
+    }()
+    
+    private let reviewContentLabel: UILabel = {
+        let label = UILabel()
+        label.text = "동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라만세 무궁화 삼천리 화려강산 대한사람 대한으로 길이 보전하세"
+        label.font = .systemFont(ofSize: 14, weight: .semibold)
+        label.textColor = .white
+        label.numberOfLines = 3
+        return label
+    }()
+    
+    private let developerResponseTextLabel: UILabel = {
+        let label = UILabel()
+        label.text = "개발자 답변"
+        label.font = .systemFont(ofSize: 12)
+        label.textColor = .lightGray
+        return label
+    }()
+    
+    private let developerResponseContentLabel: UILabel = {
+        let label = UILabel()
+        label.text = "안녕하세요. 토스팀입니다. 남산위에 저 소나무 철갑을 두른듯 바람서리 불변함은 우리기상 일세 무궁화 삼천리 화려강산 대한사람 대한으로 길이보전하세"
+        label.font = .systemFont(ofSize: 12)
+        label.textColor = .lightGray
+        return label
+    }()
+    
+    private let developerResponseDateLabel: UILabel = {
+        let label = UILabel()
+        label.text = "10월 22일"
+        label.font = .systemFont(ofSize: 12)
+        label.textColor = .lightGray
+        return label
+    }()
+    
+    
+    // 별 버튼 5개 생성 (탭하여 평가하기)
+    private func createReviewStarsButton() -> UIButton {
+        let button = UIButton()
+        let starImage = UIImage(systemName: "star", withConfiguration: UIImage.SymbolConfiguration(pointSize: 18))
+        button.setImage(starImage, for: .normal)
+        button.tintColor = .systemBlue
+        button.imageView?.contentMode = .scaleAspectFit
+    
+        return button
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -368,6 +526,11 @@ class AppStoreTossMainViewController: UIViewController {
     private func setUI(){
         scrollView.addSubview(contentView)
         view.addSubview(scrollView)
+        
+        for _ in 1...5 {
+            let starButton = createReviewStarsButton()
+            rateStarStackView.addArrangedSubview(starButton)
+        }
         
         [
             appImageImageView,
@@ -399,7 +562,24 @@ class AppStoreTossMainViewController: UIViewController {
             developerTextLabel,
             devloperInfoTextButton,
             developerAppMoreButton,
-            separateLine5
+            separateLine5,
+            
+            ratingAndReviewLabel,
+            ratingScoreLabel,
+            maxRatingLabel,
+            seeAllReviewsButton,
+            reviewCountsLabel,
+            separateLine6,
+            
+            tapToRateLabel,
+            rateStarStackView,
+            
+            reviewBackgroundGrayView,
+            reviewTitleLabel,
+            reviewDateLabel,
+            reviewStarScoreLabel,
+            userNameLabel,
+            reviewContentLabel,
         ].forEach {
             $0.translatesAutoresizingMaskIntoConstraints = false
             contentView.addSubview($0)
@@ -417,8 +597,8 @@ class AppStoreTossMainViewController: UIViewController {
             ageStackView.addArrangedSubview($0)
         }
         
-        
         infoStackView.addArrangedSubviews(rateStackView, verticalLine1, awardStackView, verticalLine2, ageStackView)
+        
         
     }
     
@@ -589,10 +769,94 @@ class AppStoreTossMainViewController: UIViewController {
         }
         
         separateLine5.snp.makeConstraints {
-            $0.top.equalTo(developerTextLabel.snp.bottom).offset(20)
+            $0.top.equalTo(developerTextLabel.snp.bottom).offset(30)
             $0.leading.trailing.equalToSuperview().inset(20)
             $0.height.equalTo(0.3)
+        }
+        
+        // MARK: 여섯번째 섹션 (평가 및 리뷰 탭)
+        ratingAndReviewLabel.snp.makeConstraints {
+            $0.top.equalTo(separateLine5.snp.bottom).offset(20)
+            $0.leading.equalToSuperview().inset(20)
+        }
+        
+        seeAllReviewsButton.snp.makeConstraints{
+            $0.centerY.equalTo(ratingAndReviewLabel.snp.centerY)
+            $0.trailing.equalToSuperview().inset(20)
+        }
+        
+        ratingScoreLabel.snp.makeConstraints {
+            $0.top.equalTo(ratingAndReviewLabel.snp.bottom)
+            $0.leading.equalToSuperview().inset(20)
+        }
+        
+        maxRatingLabel.snp.makeConstraints {
+            $0.top.equalTo(ratingScoreLabel.snp.bottom)
+            $0.centerX.equalTo(ratingScoreLabel.snp.centerX)
+        }
+        
+        reviewCountsLabel.snp.makeConstraints{
+            $0.centerY.equalTo(ratingScoreLabel.snp.centerY)
+            $0.trailing.equalToSuperview().inset(20)
+        }
+        
+        separateLine6.snp.makeConstraints {
+            $0.top.equalTo(maxRatingLabel.snp.bottom).offset(20)
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.height.equalTo(0.4) // ??
+        }
+        
+        // 리뷰 부분
+        tapToRateLabel.snp.makeConstraints {
+            $0.top.equalTo(separateLine6.snp.bottom).offset(20)
+            $0.leading.equalToSuperview().inset(20)
+        }
+        
+        rateStarStackView.snp.makeConstraints {
+            $0.centerY.equalTo(tapToRateLabel.snp.centerY)
+            $0.leading.equalTo(tapToRateLabel.snp.trailing).offset(10)
+            $0.trailing.lessThanOrEqualToSuperview().inset(20)
+            $0.width.equalTo(200)
+            $0.height.equalTo(40)
+        }
+
+        // 리뷰
+        reviewBackgroundGrayView.snp.makeConstraints{
+            $0.top.equalTo(tapToRateLabel.snp.bottom).offset(20)
+            $0.centerX.equalToSuperview()
+            $0.height.equalTo(200)
+            $0.leading.trailing.equalToSuperview().inset(15)
             $0.bottom.equalTo(contentView.snp.bottom)
+
+        }
+        
+        reviewTitleLabel.snp.makeConstraints{
+            $0.top.equalTo(reviewBackgroundGrayView).offset(15)
+            $0.leading.equalTo(reviewBackgroundGrayView).offset(15)
+        }
+        
+        reviewDateLabel.snp.makeConstraints{
+            $0.trailing.equalTo(reviewBackgroundGrayView).inset(15)
+            $0.centerY.equalTo(reviewTitleLabel.snp.centerY)
+        }
+        
+        reviewStarScoreLabel.snp.makeConstraints{
+            $0.top.equalTo(reviewTitleLabel.snp.bottom).offset(3)
+            $0.leading.equalTo(reviewTitleLabel.snp.leading)
+        }
+        
+        userNameLabel.snp.makeConstraints{
+            $0.trailing.equalTo(reviewBackgroundGrayView).inset(15)
+            $0.centerY.equalTo(reviewStarScoreLabel.snp.centerY)
+        }
+        
+        reviewContentLabel.snp.makeConstraints{
+            $0.top.equalTo(reviewStarScoreLabel.snp.bottom).offset(10)
+            $0.leading.equalTo(reviewStarScoreLabel.snp.leading)
+            $0.trailing.equalTo(reviewBackgroundGrayView).inset(15)
         }
     }
+    
+    
+    
 }
