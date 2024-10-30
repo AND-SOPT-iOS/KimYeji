f40a645e8e967e67d978429631ab9a1c27d2b838
Yeji <mnbvcxzyj@gmail.com>
2024-10-31 01:40:30 +0900

    add: 앱 아이콘 이미지 추가

---
+++ AND-SOPT-iOS/AND-SOPT-iOS/Assignment/Week03/FinancialTab/FinancialTabViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FinancialTabViewController.swift
+//  AND-SOPT-iOS
+//
+//  Created by 김예지 on 10/31/24.
+//
+
+import UIKit
+
+class FinancialTabViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+
+}
---
+++ AND-SOPT-iOS/AND-SOPT-iOS/Assignment/Week03/FinancialTab/View/RecommendationView.swift
@@ -0,0 +1,32 @@
+//
+//  RecommendationView.swift
+//  AND-SOPT-iOS
+//
+//  Created by 김예지 on 10/31/24.
+//
+
+import UIKit
+
+class RecommendationView: UIView {
+
+    // MARK: - Initializer
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - UI, Layout
+    private func setUI() {
+        
+    }
+    
+    private func setLayout(){
+        
+    }
+
+}
