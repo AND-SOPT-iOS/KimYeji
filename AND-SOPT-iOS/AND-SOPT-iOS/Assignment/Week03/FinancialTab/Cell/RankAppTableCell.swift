ae239d6034ff4aba5ff6171678390887a893c438
Yeji <mnbvcxzyj@gmail.com>
2024-11-01 05:40:04 +0900

    feat: 인기차트 테이블뷰 셀 UI

---
+++ AND-SOPT-iOS/AND-SOPT-iOS/Assignment/Week03/FinancialTab/Cell/RankAppTableCell.swift
@@ -0,0 +1,23 @@
+//
+//  RankAppTableCell.swift
+//  AND-SOPT-iOS
+//
+//  Created by 김예지 on 11/1/24.
+//
+
+import UIKit
+
+class RankAppTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
