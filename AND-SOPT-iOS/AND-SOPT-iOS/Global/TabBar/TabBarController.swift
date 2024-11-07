ea92f06e377736203ed715cb5b3187e41e7ce1a5
Yeji <mnbvcxzyj@gmail.com>
2024-11-07 22:54:23 +0900

    feat: 탭 바 생성

---
+++ AND-SOPT-iOS/AND-SOPT-iOS/Global/TabBar/TabBarController.swift
@@ -0,0 +1,33 @@
+//
+//  TabBarController.swift
+//  AND-SOPT-iOS
+//
+//  Created by 김예지 on 11/7/24.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTabBar()
+    }
+    
+    // MARK: - TabBar 설정
+    private func setupTabBar() {
+        let financialTabMainViewController = FinancialTabMainViewController()
+        let chartViewController = ChartViewController()
+        let hobbyViewController = HobbyViewController()
+        
+        let financialNavicationController = UINavigationController(rootViewController: financialTabMainViewController)
+        let chartNavigationController = UINavigationController(rootViewController: chartViewController)
+        let hobbyNavigationController = UINavigationController(rootViewController: hobbyViewController)
+        
+        financialNavicationController.tabBarItem = UITabBarItem(title: "금융", image: UIImage(systemName: "wonsign"), tag: 0)
+        chartNavigationController.tabBarItem = UITabBarItem(title: "차트", image: UIImage(systemName: "chart.bar.doc.horizontal"), tag: 1)
+        hobbyNavigationController.tabBarItem = UITabBarItem(title: "서버", image: UIImage(systemName: "heart.fill"), tag: 2)
+        
+        self.viewControllers = [financialNavicationController, chartNavigationController, hobbyNavigationController]
+    }
+}
